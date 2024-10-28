1bf47bd6767ddd6a8c6282c419aef695a7fcfba7
Joseph D <751728@student.dusd.net>
2024-10-28 10:23:29 -0700

    worked on it
    Added new scripts

---
+++ ScriptingProjectP2Diaz/Assets/Scripts/DoWhileLoops.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+
+public class DoWhileLoops : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool shouldContinue = false;
+
+        do
+        {
+            print("Hello World");
+
+        } while (shouldContinue == true);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
