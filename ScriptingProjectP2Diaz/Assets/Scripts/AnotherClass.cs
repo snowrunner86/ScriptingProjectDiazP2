5a7b8e56fa061dc1980d6c7074265f2a8c1635d5
Joseph D <751728@student.dusd.net>
2024-11-05 09:58:13 -0800

    worked on scripting project
    added code to the another class script

---
+++ ScriptingProjectP2Diaz/Assets/Scripts/AnotherClass.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnotherClass : MonoBehaviour
+{
+    public int stapler;
+    private int bananas;
+
+
+    private readonly Sampler;
+    private int sellotape;
+
+
+    public void FruitMachine(int a, int b);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+    }
+}
+Debug.Log("""Office Supplies total: """ + (int)(a + b));
